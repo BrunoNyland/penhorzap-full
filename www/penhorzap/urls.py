55e5f58ce20d3f1068b3277dc9d64cd82ac3af58
Bruno <bruno-nyland@hotmail.com>
2026-07-20 12:32:16 -0400

    fix: unifica as páginas de login para o angular

--- www/penhorzap/urls.py
+++ www/penhorzap/urls.py
@@ -3,9 +3,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
+from django.shortcuts import redirect
 
-admin.site.login_template = "admin/login_penhorzap.html"
-admin.site.site_header = "penhorzap"
+admin.site.login = lambda request, extra_context=None: redirect("/painel/")
+admin.site.site_header = "PenhorZap"
 
 urlpatterns = [
     path("", RedirectView.as_view(url="/painel/", permanent=False)),
@@ -15,4 +16,5 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
